c1d6e8d6b97b296520cbc3039cc26a80d6bcd7c9
rahul <rcsofttech85@gmail.com>
2023-09-11 23:45:33 +0530

    file integrity checker Signed-off-by: rahul <rcsofttech85@gmail.com>
    Signed-off-by: rahul <rcsofttech85@gmail.com>

--- src/Exception/FileEncryptorException.php
+++ src/Exception/FileEncryptorException.php
@@ -7,7 +7,7 @@ use Throwable;
 
 class FileEncryptorException extends Exception
 {
-    public function __construct($message = "could not encrypt file", $code = 0, Throwable $previous = null)
+    public function __construct(string $message = "could not encrypt file", int $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
