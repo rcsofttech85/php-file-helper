856d709de77b0afa8a62487345dbfc9fdb85117f
rahul <rcsofttech85@gmail.com>
2023-09-22 09:32:15 +0530

    reduce complexity for getRows method
    Signed-off-by: rahul <rcsofttech85@gmail.com>

--- src/JsonFileHandler.php
+++ src/JsonFileHandler.php
@@ -29,36 +29,41 @@ readonly class JsonFileHandler
 
     /**
      * @param string $filename
-     * @param array<string> $headers
-     * @param array<string>|false $hideColumns
-     * @param int|false $limit
-     * @return Generator
+     * @return array<int,array<string,string>>
      * @throws FileHandlerException
      */
-    private function getRows(
-        string $filename,
-        array &$headers,
-        array|false $hideColumns = false,
-        int|false $limit = false
-    ): Generator {
+    private function validateFile(string $filename): array
+    {
         if (!file_exists($filename)) {
             throw new FileHandlerException('file not found');
         }
+
         $jsonContents = file_get_contents($filename);
 
         if (!$jsonContents) {
             throw new FileHandlerException("{$filename} is not valid");
         }
 
-
         if (!$contents = $this->isValidJson($jsonContents)) {
             throw new FileHandlerException(json_last_error_msg());
         }
 
+        return $contents;
+    }
 
+    /**
+     * @param array<int,array<string,string>> $contents
+     * @param array<int<0, max>, int> $indices
+     * @param int|false $limit
+     * @return Generator
+     */
+
+    private function getProcessedContent(
+        array $contents,
+        array $indices,
+        int|false $limit = false
+    ): Generator {
         $count = 0;
-        $headers = array_keys($contents[0]);
-        $indices = is_array($hideColumns) ? $this->setColumnsToHide($headers, $hideColumns) : [];
         foreach ($contents as $content) {
             if (!empty($indices)) {
                 $content = array_values($content);
@@ -72,6 +77,29 @@ readonly class JsonFileHandler
             }
         }
     }
+
+    /**
+     * @param string $filename
+     * @param array<string> $headers
+     * @param array<string>|false $hideColumns
+     * @param int|false $limit
+     * @return Generator
+     * @throws FileHandlerException
+     */
+    public function getRows(
+        string $filename,
+        array &$headers,
+        array|false $hideColumns = false,
+        int|false $limit = false
+    ): Generator {
+        $contents = $this->validateFile($filename);
+
+        $headers = array_keys($contents[0]);
+        $indices = is_array($hideColumns) ? $this->setColumnsToHide($headers, $hideColumns) : [];
+
+        return $this->getProcessedContent($contents, $indices, $limit);
+    }
+
 
     /**
      * @param string $jsonData
