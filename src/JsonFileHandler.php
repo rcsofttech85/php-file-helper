6f8027a83f6586bc6613e110216059d4f298c1d6
rahul <rcsofttech85@gmail.com>
2023-09-22 09:32:15 +0530

    reduce complexity for getRows method
    Signed-off-by: rahul <rcsofttech85@gmail.com>

--- src/JsonFileHandler.php
+++ src/JsonFileHandler.php
@@ -6,7 +6,7 @@ use Generator;
 use rcsofttech85\FileHandler\Exception\FileHandlerException;
 use rcsofttech85\FileHandler\Utilities\RowColumnHelper;
 
-readonly class JsonFileHandler
+class JsonFileHandler
 {
     use RowColumnHelper;
 
@@ -15,7 +15,7 @@ readonly class JsonFileHandler
      * @param array<string> $headers
      * @param array<string>|false $hideColumns
      * @param int|false $limit
-     * @return array<string,string>
+     * @return array<int,array<string,string>>
      * @throws FileHandlerException
      */
     public function toArray(
@@ -29,69 +29,70 @@ readonly class JsonFileHandler
 
     /**
      * @param string $filename
-     * @param array<string> $headers
-     * @param array<string>|false $hideColumns
-     * @param int|false $limit
-     * @return Generator
+     * @return array<int,array<string,string>>
      * @throws FileHandlerException
      */
-    private function getRows(
-        string $filename,
-        array &$headers,
-        array|false $hideColumns = false,
-        int|false $limit = false
-    ): Generator {
-        if (!file_exists($filename)) {
-            throw new FileHandlerException('file not found');
-        }
-        $jsonContents = file_get_contents($filename);
 
+    private function validateFile(string $filename): array
+    {
+        $this->checkFileExistence($filename);
+        $jsonContents = $this->getFileContents($filename);
+        $contents = $this->parseJson($jsonContents);
+        if (!$contents) {
+            throw new FileHandlerException('could not parse json');
+        }
+        $this->validateJsonData($contents);
+        return $contents;
+    }
+
+    /**
+     * @param string $filename
+     * @return void
+     * @throws FileHandlerException
+     */
+    private function checkFileExistence(string $filename): void
+    {
+        if (!file_exists($filename)) {
+            throw new FileHandlerException('File not found');
+        }
+    }
+
+    /**
+     * @param string $filename
+     * @return string
+     * @throws FileHandlerException
+     */
+    private function getFileContents(string $filename): string
+    {
+        $jsonContents = file_get_contents($filename);
         if (!$jsonContents) {
             throw new FileHandlerException("{$filename} is not valid");
         }
-
-
-        if (!$contents = $this->isValidJson($jsonContents)) {
-            throw new FileHandlerException(json_last_error_msg());
-        }
-
-
-        $count = 0;
-        $headers = array_keys($contents[0]);
-        $indices = is_array($hideColumns) ? $this->setColumnsToHide($headers, $hideColumns) : [];
-        foreach ($contents as $content) {
-            if (!empty($indices)) {
-                $content = array_values($content);
-                $this->removeElementByIndex($content, $indices);
-            }
-            yield $content;
-            $count++;
-
-            if (is_int($limit) && $limit <= $count) {
-                break;
-            }
-        }
+        return $jsonContents;
     }
 
     /**
      * @param string $jsonData
      * @return array<int,array<string,string>>|false
      */
-    private function isValidJson(string $jsonData): array|false
+    private function parseJson(string $jsonData): array|false
     {
         $data = json_decode($jsonData, true);
-
-        if (json_last_error() !== JSON_ERROR_NONE) {
+        if (json_last_error() !== JSON_ERROR_NONE || !is_array($data)) {
             return false;
         }
+        return $data;
+    }
 
-
-        if (!is_array($data)) {
-            return false;
-        }
-
-        if (!isset($data[0]) || !is_array($data[0])) {
-            return false;
+    /**
+     * @param array<int,array<string,string>>|false $data
+     * @return void
+     * @throws FileHandlerException
+     */
+    private function validateJsonData(array|false $data): void
+    {
+        if (empty($data) || !is_array($data[0])) {
+            throw new FileHandlerException(json_last_error_msg());
         }
 
         $firstArrayKeys = array_keys($data[0]);
@@ -100,10 +101,56 @@ readonly class JsonFileHandler
             $currentArrayKeys = array_keys($item);
 
             if ($firstArrayKeys !== $currentArrayKeys) {
-                return false;
+                throw new FileHandlerException('Inconsistent JSON data');
             }
         }
+    }
 
-        return $data;
+    /**
+     * @param array<int,array<string,string>> $contents
+     * @param array<int<0,max>,int> $indices
+     * @param int|false $limit
+     * @return Generator
+     */
+    private function getProcessedContent(array $contents, array $indices, int|false $limit = false): Generator
+    {
+        $count = 0;
+        $shouldLimit = is_int($limit);
+
+        foreach ($contents as $content) {
+            if (!empty($indices)) {
+                $content = array_values($content);
+                $this->removeElementByIndex($content, $indices);
+            }
+
+            yield $content;
+            $count++;
+
+            if ($shouldLimit && $count >= $limit) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * @param string $filename
+     * @param array<string> $headers
+     * @param array<string,string>|false $hideColumns
+     * @param int|false $limit
+     * @return Generator
+     * @throws FileHandlerException
+     */
+    public function getRows(
+        string $filename,
+        array &$headers,
+        array|false $hideColumns = false,
+        int|false $limit = false
+    ): Generator {
+        $contents = $this->validateFile($filename);
+
+        $headers = array_keys($contents[0]);
+        $indices = is_array($hideColumns) ? $this->setColumnsToHide($headers, $hideColumns) : [];
+
+        return $this->getProcessedContent($contents, $indices, $limit);
     }
 }
