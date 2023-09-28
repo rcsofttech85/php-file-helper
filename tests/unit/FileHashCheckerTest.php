873fdd6051e2891761935fa17a5228dbe3c3b648
rahul <rcsofttech85@gmail.com>
2023-09-28 22:32:49 +0530

    store new hash for integrity check
    Signed-off-by: rahul <rcsofttech85@gmail.com>

--- tests/unit/FileHashCheckerTest.php
+++ tests/unit/FileHashCheckerTest.php
@@ -10,7 +10,6 @@ use Rcsofttech85\FileHandler\FileHashChecker;
 
 class FileHashCheckerTest extends BaseTest
 {
-    private static string $file;
     private FileHashChecker|null $fileHash = null;
 
 
@@ -31,30 +30,27 @@ class FileHashCheckerTest extends BaseTest
     {
         parent::setUpBeforeClass();
 
-        $file = self::$containerBuilder->getParameter('STORED_HASH_FILE');
-        if (is_string($file)) {
-            self::$file = $file;
-        }
         static::$files[] = 'sample';
     }
 
 
     /**
      * @throws HashException
+     * @throws FileHandlerException
      */
     #[Test]
     public function shouldGenerateValidHashForDifferentAlgo(): void
     {
         $expectedHash = "5923032f7e18edf69e1a3221be3205ce658ec0e4fb274016212a09a804240683";
 
-        $actualHash = $this->fileHash->hashFile(); //default ALGO_256
+        $actualHash = $this->fileHash->hashFile(filename: self::$files[0]); //default ALGO_256
 
         $this->assertEquals($expectedHash, $actualHash);
 
         $expectedHash = "1050bcc2d7d840d634f067a22abb4cd693b1f2590849982e29a6f9bb28963f733" .
             "92b63ea24ae17edfaa500ee62b9e5482b9648af0b2b7d941992af3b0f9cbd3b";
 
-        $actualHash = $this->fileHash->hashFile(FileHashChecker::ALGO_512);
+        $actualHash = $this->fileHash->hashFile(filename: self::$files[0], algo: FileHashChecker::ALGO_512);
 
         $this->assertEquals($expectedHash, $actualHash);
     }
@@ -66,7 +62,7 @@ class FileHashCheckerTest extends BaseTest
     #[Test]
     public function checkFileIntegrityReturnsTrueIfHashMatch(): void
     {
-        $isVerified = $this->fileHash->verifyHash(storedHashesFile: self::$file);
+        $isVerified = $this->fileHash->verifyHash(filename: self::$files[0]);
 
         $this->assertTrue($isVerified);
     }
@@ -81,7 +77,7 @@ class FileHashCheckerTest extends BaseTest
         $backup = file_get_contents("movie.csv");
         file_put_contents("movie.csv", "modified", FILE_APPEND);
 
-        $isVerified = $this->fileHash->verifyHash(self::$file);
+        $isVerified = $this->fileHash->verifyHash(self::$files[0]);
 
         $this->assertfalse($isVerified);
 
@@ -95,8 +91,24 @@ class FileHashCheckerTest extends BaseTest
     #[Test]
     public function shouldReturnFalseIfDifferentAlgoIsUsedForVerifyHash(): void
     {
-        $isVerified = $this->fileHash->verifyHash(self::$file, FileHashChecker::ALGO_512);
+        $isVerified = $this->fileHash->verifyHash(self::$files[0], FileHashChecker::ALGO_512);
 
         $this->assertFalse($isVerified);
+    }
+
+    /**
+     * @throws HashException
+     * @throws FileHandlerException
+     */
+    #[Test]
+    public function shouldAddRecordIfNewFileIsHashed(): void
+    {
+        file_put_contents('sample', "hello");
+
+        $this->fileHash->hashFile(self::$files[1], FileHashChecker::ALGO_512);
+
+        $isVerified = $this->fileHash->verifyHash(self::$files[1], FileHashChecker::ALGO_512);
+
+        $this->assertTrue($isVerified);
     }
 }
