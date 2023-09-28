cb0ba07cdefa9a9bf5323150229583bd68c2eca4
rcsofttech85 <rcsofttech85@gmail.com>
2023-09-28 23:06:02 +0530

    store new hash for integrity check (#47)
    Signed-off-by: rahul <rcsofttech85@gmail.com>

--- src/FileHashChecker.php
+++ src/FileHashChecker.php
@@ -2,6 +2,7 @@
 
 namespace Rcsofttech85\FileHandler;
 
+use Rcsofttech85\FileHandler\Exception\FileHandlerException;
 use Rcsofttech85\FileHandler\Exception\HashException;
 use Rcsofttech85\FileHandler\Validator\FileValidatorTrait;
 
@@ -9,46 +10,47 @@ class FileHashChecker
 {
     use FileValidatorTrait;
 
+
     public const ALGO_256 = 'sha3-256';
     public const ALGO_512 = 'sha3-512';
 
+    private const SEARCH_COLUMN_NAME = 'File';
+    private const SEARCH_COLUMN_VALUE = 'Hash';
+
+
     /**
-     * @param string $filename
      * @param CsvFileHandler $csvFileHandler
-     * @throws Exception\FileHandlerException
      */
-    public function __construct(private string $filename, private readonly CsvFileHandler $csvFileHandler)
+    public function __construct(private readonly CsvFileHandler $csvFileHandler)
     {
-        $this->filename = $this->validateFileName($filename);
     }
 
     /**
-     * @param string $storedHashesFile
+     * @param string $filename
      * @param string $algo
      * @return bool
-     * @throws Exception\FileHandlerException
+     * @throws FileHandlerException
      * @throws HashException
      */
 
-    public function verifyHash(string $storedHashesFile, string $algo = self::ALGO_256): bool
+    public function verifyHash(string $filename, string $algo = self::ALGO_256): bool
     {
-        if (!$storedHashesFile) {
-            throw new HashException('file not found');
-        }
-
+        $storedHashesFile = $this->getParameter(self::STORED_HASH_FILE);
         $file = $this->csvFileHandler->searchInCsvFile(
             filename: $storedHashesFile,
-            keyword: $this->filename,
-            column: 'File',
+            keyword: $filename,
+            column: self::SEARCH_COLUMN_NAME,
             format: FileHandler::ARRAY_FORMAT
         );
+
 
         if (!$file || !is_array($file)) {
             throw new HashException('this file is not hashed');
         }
 
         $expectedHash = $file['Hash'];
-        $hash = $this->hashFile($algo);
+        $hash = $this->hashFile($filename, $algo);
+
 
         if ($hash !== $expectedHash) {
             return false;
@@ -58,16 +60,65 @@ class FileHashChecker
     }
 
     /**
+     * @param string $filename
      * @param string $algo
      * @return string
-     * @throws HashException
+     * @throws HashException|FileHandlerException
      */
 
-    public function hashFile(string $algo = self::ALGO_256): string
+    public function hashFile(string $filename, string $algo = self::ALGO_256): string
     {
+        $this->validateFileName($filename);
         if (!in_array($algo, [self::ALGO_512, self::ALGO_256])) {
             throw new HashException('algorithm not supported');
         }
-        return hash_file($algo, $this->filename);
+
+        if (!$hash = hash_file($algo, $filename)) {
+            throw new HashException('could not hash file');
+        }
+
+        $storedHashesFile = $this->getParameter(self::STORED_HASH_FILE);
+
+
+        $file = fopen($storedHashesFile, 'a+');
+        if (!$file) {
+            throw new FileHandlerException('file not found');
+        }
+        $this->checkHeaderExists($file);
+
+
+        try {
+            $filenameExists = $this->csvFileHandler->searchInCsvFile(
+                filename: $storedHashesFile,
+                keyword: $filename,
+                column: 'File'
+            );
+
+            if (!$filenameExists) {
+                fputcsv($file, [$filename, $hash]);
+            }
+        } catch (FileHandlerException) {
+            fputcsv($file, [$filename, $hash]);
+        } finally {
+            fclose($file);
+        }
+
+
+        return $hash;
+    }
+
+    /**
+     * @param mixed $storedHashFile
+     * @return void
+     */
+    private function checkHeaderExists(mixed $storedHashFile): void
+    {
+        $header = fgetcsv($storedHashFile);
+
+        if (!$header || $header[0] !== self::SEARCH_COLUMN_NAME || $header[1] !== self::SEARCH_COLUMN_VALUE) {
+            fseek($storedHashFile, 0);
+            fputcsv($storedHashFile, [self::SEARCH_COLUMN_NAME, self::SEARCH_COLUMN_VALUE]);
+            fflush($storedHashFile);
+        }
     }
 }
